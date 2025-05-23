73f71a5d83d3149b9eb66c338a99b8580a69df9f
kronos <eliasvieiraresende23@gmail.com>
2025-05-22 21:16:04 -0300

    Trying to get the rigth width for the grid

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,17 @@
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 2700; i++) {
+
+
+let qntQuadrados = 16;
+
+for (let i = 0; i < qntQuadrados * qntQuadrados; i++) {
     const div = document.createElement("div")
-    div.setAttribute("id", "square")
+    div.classList.add("square")
     container.appendChild(div)
 }
 
-const square = document.querySelector("#square")
+function getWidth() {
+    return qntQuadrados / 950
+}
 
-square.addEventListener('mouseon', () => {
-})
+div.style.cssText = "width: " + getWidth + "; heigth: " + getWidth + ";"
